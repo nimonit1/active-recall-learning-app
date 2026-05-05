27e9ec27a7d64d1e2a095c85e654ac562ba38373
nimo <nimonit1@gmail.com>
2026-05-05 12:23:15 +0900

    デブロイのgithub actionのworkflowを設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/active-recall-learning-app',
   plugins: [react()],
   test: {
     globals: true,
